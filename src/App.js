f19ebb1bf7bfd0106181b1a38e5e127e434613e8
AnesuMuusha <anesumuusha@gmail.com>
2024-04-03 15:16:32 +0200

    fetched and displayed sample date, installed axios.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
     <div className="text-6xl">Home Page</div>
+      <HomePage/>
     </div>
   );
 }
