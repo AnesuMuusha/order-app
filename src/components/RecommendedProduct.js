187786678b8b744d6aa40efb71463c097b7c06af
AnesuMuusha <anesumuusha@gmail.com>
2024-04-04 12:55:30 +0200

    Added order names and IDs, Justified images

--- src/components/RecommendedProduct.js
+++ src/components/RecommendedProduct.js
@@ -3,7 +3,7 @@ import React from "react";
 const RecommendedProduct = ({ recommendedProduct }) => {
   return (
     
-    <div className="flex flex-col px-4 py-6 md:p-6 w-full bg-gray-50 space-y-6 mt-6">
+      <div className="flex flex-col px-4 py-6 md:p-6 w-full bg-gray-50 space-y-6 mt-6 items-center">
       
       <img src={recommendedProduct.imageUrl} alt="recommended product" className="m-2 h-24 w-28 rounded-md border object-cover object-center"
         />
--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 const Orders = () => {
   const orders = [
-    "65f1888eca763954f8e1ca06",
-    "65f190c3ff993bb6fbcde822",
-    "65fae76c0ff63ef14669a6b6",
+    {name:"Order one", id:"65f1888eca763954f8e1ca06"},
+    {name:"Order two", id:"65f190c3ff993bb6fbcde822"},
+    {name:"Order three", id:"65fae76c0ff63ef14669a6b6"},
   ];
 
   return (
     <>
-      <h4 className="text-xl font-medium my-8">Orders</h4>
+      <h4 className="text-xl  my-8 font-bold">Orders</h4>
 
       <ul>
         {orders.map((order, index) => (
-          <li key={index}>
-            <Link to={`/orders/${order}`}>{order}</Link>
+          <li key={order.id} className="my-8 text-md font-semibold ">
+            <Link to={`/orders/${order.id}`}>{order.name}</Link>
 
           </li>
         ))}
